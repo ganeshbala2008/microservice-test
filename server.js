c3c1a636dc753ae51ef7db79be18a7b6844e9a2f
Ganesh Balajii <ganesh.balajii@externals.adidas.com>
2022-03-15 17:25:58 +0530

    Route added

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 const port = 8080;
 const host = '0.0.0.0';
 
+app.get('/', (req, res) => {
+ res.send('testing');
+})
 app.get('/hello', (req, res) => {
   res.send('Hello Ganesh Balajii!');
 })
